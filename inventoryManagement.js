8e30f18983c0f12e2a5d339ac6604f43f46d1107
Solo <solomonmaguta492@gmail.com>
2026-09-25 23:27:48 -1000

    added required tasks

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,9 +1,28 @@
 // Write your code here
+const products = ["Laptop" , "Phone" , "Headphone" , "Monitor"]
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(newProduct) {
+  products.push(newProduct);
+  console.log(products)
+}
+
+function updateProductName(index , newName) {
+  products[index] = newName;
+  console.log(products);
+}
+
+function removeLastProduct() {
+  products.pop();
+  console.log(products);
+}
 
 
 // Export the necessary parts for testing
-module.exports = {
+module.exports =  {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
